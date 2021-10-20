123755311394a6846acb372bc7ee357d9a0e1dad
Seamus O'Sullivan <info@seamuso.com>
2021-10-21 01:01:25 +0200

    further contentful artist tests

---
+++ pages/artists/[slug].js
@@ -0,0 +1,3 @@
+export default function ArtistDetails() {
+  return <div>Artist Details</div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,44 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.SPACE_ID,
+    accessToken: process.env.ACCESS_TOKEN,
+  });
+
+  const res = await client.getEntries({ content_type: "artist" });
+
+  return {
+    props: {
+      artist: res.items,
+    },
+  };
+}
+
+export default function Recipes({ artist }) {
+  //console.log(artist.sys.id, "here is artist");
   return (
     <div className="recipe-list">
-      Recipe List
+      artist List
+      {artist.map((artist) => (
+        <div key={artist.sys.id}>
+          {console.log(
+            artist.fields.thumbnail.fields.file.url,
+            "here is the thumbnail"
+          )}
+          {artist.fields.title}
+          <img
+            src={artist.fields.thumbnail.fields.file.url}
+            height="300px"
+            width="350px"
+          />
+          <img
+            src={artist.fields.thumbnail.fields.file.url}
+            height="300px"
+            width="350px"
+          />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
