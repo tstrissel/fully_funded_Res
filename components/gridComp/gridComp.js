e06e08d15b7bfacc242c2ef7e2d5904950f24c17
Seamus O'Sullivan <info@seamuso.com>
2021-10-27 15:41:39 +0200

    grid created

--- components/gridComp/gridComp.js
+++ components/gridComp/gridComp.js
@@ -1,27 +1,31 @@
+import styles from "./gridComp.module.css";
+
 export default function GridComp({ artist }) {
-//   console.log(artist, "ARTIST HERE");
+  //   console.log(artist, "ARTIST HERE");
   return (
     <div>
       Artist List
-      {artist.map((artist) => (
-        <div key={artist.sys.id}>
-          {console.log(
-            artist.fields.featuredImage.fields.file.url,
-            "here is the thumbnail"
-          )}
-          {artist.fields.title}
-          <img
-            src={artist.fields.thumbnail.fields.file.url}
-            height="300px"
-            width="350px"
-          />
-          <img
-            src={artist.fields.featuredImage.fields.file.url}
-            height="300px"
-            width="350px"
-          />
-        </div>
-      ))}
+      <ul className={styles.wrapper}>
+        {artist.map((artist) => (
+          <div key={artist.sys.id}>
+            {console.log(
+              artist.fields.featuredImage.fields.file.url,
+              "here is the thumbnail"
+            )}
+            {artist.fields.title}
+            <img
+              src={artist.fields.thumbnail.fields.file.url}
+              height="300px"
+              width="350px"
+            />
+            {/* <img
+              src={artist.fields.featuredImage.fields.file.url}
+              height="300px"
+              width="350px"
+            /> */}
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
