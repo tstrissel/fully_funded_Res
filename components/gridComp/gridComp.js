935f18cd47855b617be02a8de646bba146e8edf0
Seamus O'Sullivan <info@seamuso.com>
2022-01-03 18:06:49 +0100

    modal created

--- components/gridComp/gridComp.js
+++ components/gridComp/gridComp.js
@@ -1,11 +1,14 @@
 import styles from "./gridComp.module.css";
 import Link from "next/link";
+import SearchModal from "../SearchModal/SearchModal";
+
 import { useState } from "react";
 
 export default function GridComp({ artist }) {
   const [searchTerm, setSearchTerm] = useState("");
   const [toggleViewMode, setToggleViewMode] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const [buttonPopup, setButtonPopup] = useState(false);
 
   const handleClick = () => {
     isOpen === false ? setIsOpen(true) : setIsOpen(false);
@@ -14,6 +17,15 @@ export default function GridComp({ artist }) {
 
   return (
     <div>
+      <h1>Here is the modal page</h1>
+
+      <button onClick={() => setButtonPopup(true)}>Open Popup</button>
+
+      <SearchModal trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h3>HERE I AM!</h3>
+        <h2>This is my button triggered popup</h2>
+      </SearchModal>
+
       <button onClick={() => setToggleViewMode(!toggleViewMode)}>
         {toggleViewMode ? "grid" : "list"}
       </button>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { createClient } from "contentful";
 import GridComp from "../components/gridComp/gridComp";
+import SearchModal from "../components/SearchModal/SearchModal";
 // import SearchBar from "../components/searchBar/SearchBar";
 // import NavBar from "../components/navBar/NavBar";
 
