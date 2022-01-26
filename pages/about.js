57ed1fae9c4ff601b91dcb71aa28a38831bbb6b4
Seamus O'Sullivan <info@seamuso.com>
2022-01-26 15:24:48 +0100

    additional about pg changes

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import exampleImage from "../public/FFR-assets/Icons/profile_icon.svg";
 import patron from "../public/FFR-assets/Social links/Patreon button/Patreon.png";
@@ -20,9 +21,26 @@ export default function about() {
         applications and counselling.
       </p>
       <p> ways to support us:</p>
-
-      <Image src={patron} width={200} height={50} />
-      <Image src={payPal} width={200} height={50} />
+      <Link href="/about">
+        <Image src={patron} width={180} height={40} />
+      </Link>
+      <Link href="/about">
+        <Image src={payPal} width={180} height={50} />
+      </Link>
+      <p>
+        Fully Funded Residences is a nonprofit platform initiated in 2018 that
+        aims to provide a faster and easier overview of art residencies,
+        mobility funds, grants, awards and to share oyur experiences related to
+        these topics.
+      </p>
+      <p>
+        A small team is maintaining the website for free, and we woul like to
+        add more functionalities to improve the website's capabilities and look,
+        to buy a domain, and eventually to get ourselves a coffee so we can
+        continue to search and post amazing funded opportunities and to share it
+        with you!
+      </p>
+      <p>if you wish to help and donate to FFR, we would appreciate it!</p>
 
       <p>Thanks for your support!</p>
       <p>FFR Team:</p>
