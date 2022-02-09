f6167ceaf0fe39907f337e10338577f1c26dea5f
Seamus O'Sullivan <info@seamuso.com>
2022-02-09 11:36:59 +0100

    duplicate made

--- pages/resources.js
+++ pages/resources.js
@@ -1,41 +1,41 @@
-import Image from "next/image";
-import { createClient } from "contentful";
-import ResourcesComp from "../components/resourcesComp/resourcesComp";
+// import Image from "next/image";
+// import { createClient } from "contentful";
+// import ResourcesComp from "../components/resourcesComp/resourcesComp";
 
-export const getStaticProps = async (context) => {
-  const client = createClient({
-    space: process.env.SPACE_ID,
-    accessToken: process.env.ACCESS_TOKEN,
-  });
+// export const getStaticProps = async (context) => {
+//   const client = createClient({
+//     space: process.env.SPACE_ID,
+//     accessToken: process.env.ACCESS_TOKEN,
+//   });
 
-  const res = await client.getEntries({ content_type: "resources" });
-  const res2 = await client.getEntries({
-    content_type: "practicalAdvice",
-  });
-  const res2 = await client.getEntries({
-    content_type: "practicalAdviceAndOpportunities",
-  });
-  const res3 = await client.getEntries({
-    content_type: "practicalAdviceAndOpportunities",
-  });
-  return {
-    props: {
-      resources: res.items,
-      practicalAdviceAndOpportunities: res2.items,
-    },
-  };
-};
+//   const res = await client.getEntries({ content_type: "resources" });
+//   const res2 = await client.getEntries({
+//     content_type: "practicalAdvice",
+//   });
+//   const res2 = await client.getEntries({
+//     content_type: "practicalAdviceAndOpportunities",
+//   });
+//   const res3 = await client.getEntries({
+//     content_type: "practicalAdviceAndOpportunities",
+//   });
+//   return {
+//     props: {
+//       resources: res.items,
+//       practicalAdviceAndOpportunities: res2.items,
+//     },
+//   };
+// };
 
-export default function resources({
-  resources,
-  practicalAdviceAndOpportunities,
-}) {
-  return (
-    <div>
-      <ResourcesComp
-        resources={resources}
-        practicalAdviceAndOpportunities={practicalAdviceAndOpportunities}
-      />
-    </div>
-  );
-}
+// export default function resources({
+//   resources,
+//   practicalAdviceAndOpportunities,
+// }) {
+//   return (
+//     <div>
+//       <ResourcesComp
+//         resources={resources}
+//         practicalAdviceAndOpportunities={practicalAdviceAndOpportunities}
+//       />
+//     </div>
+//   );
+// }
